7204eda0210166bb27473be205cc18594adb8e20
Harshkhosla <harshkhosla9945@gmail.com>
2024-02-10 23:37:41 +0530

    changes

---
+++ src/components/Chapri.js
@@ -0,0 +1,23 @@
+import React from 'react';
+
+const Chapri = () => {
+  const handleYes = () => {
+    window.location.href = '/home';
+  };
+
+  const handleAlsoYes = () => {
+    window.location.href = '/sample';
+  };
+
+  return (
+    <div style={{ backgroundImage: `url('https://img.freepik.com/free-vector/cloud-background-vector-cute-desktop-wallpaper_53876-136885.jpg?w=826&t=st=1704789857~exp=1704790457~hmac=381926ab355f58892a476af4dd9c1b104192572bd7b1a38ac49528094d2ec6ce')`, backgroundSize: 'cover', height: '100vh', display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'center', padding: '0 20px' }}>
+      {/* <h2 style={{ color: '#FFFFFF', marginBottom: '20px', textAlign: 'center' }}>Would you like to go on a date with me, Muskan?</h2> */}
+      <div style={{ display: 'flex', justifyContent: 'space-between', width: '100%' }}>
+        <button onClick={handleYes} style={{ backgroundColor: '#4CAF50', color: '#FFFFFF', padding: '10px 20px', borderRadius: '5px', marginRight: '10px' }}>Yes</button>
+        <button onClick={handleAlsoYes} style={{ backgroundColor: '#4CAF50', color: '#FFFFFF', padding: '10px 20px', borderRadius: '5px' }}>Also Yes</button>
+      </div>
+    </div>
+  );
+};
+
+export default Chapri;
---
+++ src/components/Home.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Link ,useLocation} from 'react-router-dom'
+
+const Home = () => {
+  return (
+    <div>
+  dsadhs sd
+    </div>
+  )
+}
+
+export default Home
